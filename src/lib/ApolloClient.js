d19df389d8eefb55063a260119ec031edcaf44c7
Nicolas <nicolas@racchi.com>
2022-03-18 21:40:42 +0100

    Removed cache, deprecated

--- src/lib/ApolloClient.js
+++ src/lib/ApolloClient.js
@@ -1,11 +1,10 @@
-import { ApolloClient, inMemoryCache } from '@apollo/client';
+import { ApolloClient } from '@apollo/client';
 
 const LENS_API_URL = 'https://api-mumbai.lens.dev/';
 
 // This is the Apollo graphQL client config object.
 const Client = ApolloClient({
   uri: LENS_API_URL,
-  cache: inMemoryCache(),
 });
 
 export default Client;
