66261499065487f2fcb5e33b66150be544352c25
Nicolas <nicolas@racchi.com>
2022-03-19 17:36:35 +0100

    Added custom chakra theme object

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { ApolloProvider } from '@apollo/client';
 import { DAppProvider } from '@usedapp/core';
 import Client from './lib/ApolloClient';
 import Router from './Router';
 import DAppConfig from './lib/DAppConfig';
 
+import Theme from './lib/Theme';
+
 function App() {
   return (
     <DAppProvider config={DAppConfig}>
       <ApolloProvider client={Client}>
-        <ChakraProvider theme={theme}>
+        <ChakraProvider theme={Theme}>
           <Router />
         </ChakraProvider>
       </ApolloProvider>
