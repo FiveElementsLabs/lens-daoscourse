021fb1eac7ade458c6ed94ab3c0f8799cb218834
Paolo Antonio Rossi <paoloantoniorossi@gmail.com>
2022-03-21 14:25:06 +0100

    Update homepage

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -65,7 +65,7 @@ export default function Navbar() {
               fontFamily={'heading'}
               color={useColorModeValue('gray.800', 'white')}
             >
-              Logo
+              Lens DAOscourse
             </Text>
 
             <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
@@ -247,41 +247,11 @@ const MobileNavItem = ({ label, children, href }) => {
 
 const NAV_ITEMS = [
   {
-    label: 'Inspiration',
-    children: [
-      {
-        label: 'Explore Design Work',
-        subLabel: 'Trending Design to inspire you',
-        href: '#',
-      },
-      {
-        label: 'New & Noteworthy',
-        subLabel: 'Up-and-coming Designers',
-        href: '#',
-      },
-    ],
+    label: 'Home',
+    href: '/',
   },
   {
-    label: 'Find Work',
-    children: [
-      {
-        label: 'Job Board',
-        subLabel: 'Find your dream design job',
-        href: '#',
-      },
-      {
-        label: 'Freelance Projects',
-        subLabel: 'An exclusive list for contract work',
-        href: '#',
-      },
-    ],
-  },
-  {
-    label: 'Learn Design',
-    href: '#',
-  },
-  {
-    label: 'Hire Designers',
-    href: '#',
+    label: 'API Test',
+    href: '/api-test',
   },
 ];
