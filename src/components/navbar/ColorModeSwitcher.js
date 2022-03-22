33365939020cc96409abaab2e1e63576f0e48d03
Nicolas <nicolas@racchi.com>
2022-03-22 22:34:31 +0100

    Located theme switcher in Navbar folder

--- src/components/navbar/ColorModeSwitcher.js
+++ src/components/navbar/ColorModeSwitcher.js
@@ -9,12 +9,9 @@ export const ColorModeSwitcher = props => {
 
   return (
     <IconButton
-      size="md"
-      fontSize="lg"
+      fontSize='lg'
       aria-label={`Switch to ${text} mode`}
-      variant="ghost"
-      color="current"
-      marginLeft="2"
+      marginLeft='2'
       onClick={toggleColorMode}
       icon={<SwitchIcon />}
       {...props}
