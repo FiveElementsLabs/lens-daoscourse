a1cc3ecf3e744d9b016717eb7df3bf5d931472a0
Christian Kobril <christiankobril@gmail.com>
2022-03-23 17:49:11 +0100

    add hero page and mock dao cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,42 @@
-import { Text } from '@chakra-ui/react';
+import { Text, Grid, GridItem } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import { DAO_PROFILES } from '../lib/ConfigVars';
+import Hero from '../components/layout/Hero'
+import DaoCard from '../components/layout/DaoCard';
 
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
-
+      <Hero/>
+      <Grid templateColumns={'repeat(3, 1fr)'} gap={4}>
+        <GridItem>
+          <DaoCard/>
+        </GridItem>
+        <GridItem>
+          <DaoCard/>
+        </GridItem >
+        <GridItem>
+          <DaoCard/>
+        </GridItem>
+        <GridItem>
+          <DaoCard/>
+        </GridItem>
+        <GridItem>
+          <DaoCard/>
+        </GridItem >
+        <GridItem>
+          <DaoCard/>
+        </GridItem>
+        <GridItem>
+          <DaoCard/>
+        </GridItem>
+        <GridItem>
+          <DaoCard/>
+        </GridItem >
+        <GridItem>
+          <DaoCard/>
+        </GridItem>
+      </Grid>
       {DAO_PROFILES.map(Dao => (
         <Link key={Dao.address} to={`/${Dao.name}`}>
           <Text>{Dao.name}</Text>
